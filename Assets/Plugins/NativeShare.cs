f49cf6c9bf6f108aa737f7c566c8d4c3cba009d0
Christopher Maire <locura.madness@gmail.com>
2015-07-22 14:19:01 -0400

    Changed type from image/jpeg to image/png since that's what Unity outputs Fixed a couple incorrectly named vars

--- Assets/Plugins/NativeShare.cs
+++ Assets/Plugins/NativeShare.cs
@@ -17,7 +17,6 @@ public class NativeShare : MonoBehaviour {
         Share(text,screenShotPath,"");
     }
 
-    //Note: Android share only uses the text and screenshot path.
 	public void Share(string shareText, string imagePath, string url, string subject = "")
 	{
 #if UNITY_ANDROID
@@ -26,16 +25,16 @@ public class NativeShare : MonoBehaviour {
 		
 		intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
 		AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
-		AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + screenShotPath);
+		AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + imagePath);
 		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
-		intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
+		intentObject.Call<AndroidJavaObject>("setType", "image/png");
 		
-		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), shareString);
+		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), shareText);
 		
 		AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
 		
-		AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, shareString);
+		AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, subject);
 		currentActivity.Call("startActivity", jChooser);
 #elif UNITY_IOS
 		CallSocialShareAdvanced(shareText, subject, url, screenShotPath);
