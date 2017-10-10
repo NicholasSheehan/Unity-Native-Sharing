d6c871c20adebb4fa2826a6a633118989dbd7725
Gkhan <info@gokhankinay.com.tr>
2017-10-11 00:45:35 +0300

    Added more functions

--- Assets/Native Share Demo Scene/Test.cs
+++ Assets/Native Share Demo Scene/Test.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Runtime.InteropServices;
 using System.IO;
+using System;
 
 /*
  * https://github.com/ChrisMaire/unity-native-sharing
@@ -30,4 +31,104 @@ public class Test : MonoBehaviour {
 
 		NativeShare.Share(text, screenShotPath, "", "", "image/png", true, "");
     }
+
+    //---------- Helper Variables ----------//
+    private float width
+    {
+        get
+        {
+            return Screen.width;
+        }
+    }
+
+    private float height
+    {
+        get
+        {
+            return Screen.height;
+        }
+    }
+
+
+	//---------- Screenshot ----------//
+	public void Screenshot()
+	{
+		// Short way
+		StartCoroutine(GetScreenshot());
+	}
+
+    //---------- Get Screenshot ----------//
+    public IEnumerator GetScreenshot()
+    {
+        yield return new WaitForEndOfFrame();
+
+        // Get Screenshot
+        Texture2D screenshot;
+        screenshot = new Texture2D((int)width, (int)height, TextureFormat.ARGB32, false);
+        screenshot.ReadPixels(new Rect(0, 0, width, height), 0, 0, false);
+        screenshot.Apply();
+
+        // Save Screenshot
+        Save_Screenshot(screenshot);
+    }
+
+    //---------- Save Screenshot ----------//
+    private void Save_Screenshot(Texture2D screenshot)
+    {
+        string screenShotPath = Application.persistentDataPath + "/" + DateTime.Now.ToString("dd-MM-yyyy_HH:mm:ss") + "_" + ScreenshotName;
+        File.WriteAllBytes(screenShotPath, screenshot.EncodeToPNG());
+
+        // Native Share
+        StartCoroutine(DelayedShare_Image(screenShotPath));
+    }
+
+    //---------- Clear Saved Screenshots ----------//
+    public void Clear_SavedScreenShots()
+    {
+        string path = Application.persistentDataPath;
+
+        DirectoryInfo dir = new DirectoryInfo(path);
+        FileInfo[] info = dir.GetFiles("*.png");
+
+        foreach (FileInfo f in info)
+        {
+            File.Delete(f.FullName);
+        }
+    }
+
+    //---------- Delayed Share ----------//
+    private IEnumerator DelayedShare_Image(string screenShotPath)
+    {
+        while (!File.Exists(screenShotPath))
+        {
+            yield return new WaitForSeconds(.05f);
+        }
+
+        // Share
+        NativeShare_Image(screenShotPath);
+    }
+
+    //---------- Native Share ----------//
+    private void NativeShare_Image(string screenShotPath)
+    {
+        string text = "";
+        string subject = "";
+        string url = "";
+        string title = "Select sharing app";
+
+#if UNITY_ANDROID
+
+        subject = "Test subject.";
+        text = "Test text";
+#endif
+
+#if UNITY_IOS
+        subject = "Test subject.";
+        text = "Test text";
+#endif
+
+		// Share
+        NativeShare.Share(text, screenShotPath, url, subject, "image/png", true, title);
+    }
 }
+
